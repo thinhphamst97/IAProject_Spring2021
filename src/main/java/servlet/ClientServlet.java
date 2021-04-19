c1ced97614c1253c04faf7e82b08e13e6c9914d6
xxx <xxx@xx.xxx>
2021-04-18 22:51:42 -0400

    Last logged on, backend

--- src/main/java/servlet/ClientServlet.java
+++ src/main/java/servlet/ClientServlet.java
@@ -1,6 +1,10 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -19,6 +23,7 @@ public class ClientServlet extends HttpServlet {
 	private final String PAGE = "client.jsp";
 	private final int DEFAULTREQUESTS = 4;
 	int numOfRequests;
+	String logDirPath;
 
 	public ClientServlet() {
 		super();
@@ -26,13 +31,14 @@ public class ClientServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//String apacheLogPath = getServletContext().getInitParameter("apacheLogPath");
+		logDirPath = getServletContext().getInitParameter("logDirPath");
+		// String apacheLogPath = getServletContext().getInitParameter("apacheLogPath");
 		if (request.getParameter("numOfRequests") != null) {
 			numOfRequests = Integer.parseInt(request.getParameter("numOfRequests"));
 		} else {
 			numOfRequests = DEFAULTREQUESTS;
 		}
-		
+
 //		ArrayList<Thread> threadList = new ArrayList<Thread>();
 //		int i = 1;
 //		for (ClientDTO client : clientList) {
@@ -73,8 +79,62 @@ public class ClientServlet extends HttpServlet {
 //		}
 
 		ArrayList<ClientDTO> clientList = ClientDAO.getAll();
+		for (ClientDTO client : clientList) {
+			if (client.isOn()) {
+				String now = LocalDateTime.now().toString();
+				int index = now.indexOf("T") + 1;
+				now = now.substring(index, index + "17:49:05".length()) + "</br>"
+						+ now.substring(0, "2021-04-18".length());
+				client.setLastLoggedOn(now);
+			} else {
+				String time = getLastLoggedOn(client.getMac());
+				client.setLastLoggedOn(time);
+			}
+		}
 		request.setAttribute("clientList", clientList);
 		forward(PAGE, request, response);
+	}
+
+	private String getLastLoggedOn(String mac) {
+		String result = null;
+		String accessLogDirPath = "/var/www/html/ltsp/log" + File.separator + "access";
+		String logPath = accessLogDirPath + File.separator + mac + ".log";
+		String content = null;
+		if (Files.exists(Paths.get(logPath))) {
+			try {
+				content = Files.readString(Paths.get(logPath));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String[] lines = content.split("\n");
+
+			// time
+			if (lines.length > 0) {
+				String temp = lines[lines.length - 1];
+				int index = temp.indexOf(" ");
+				if (!temp.equals("")) {
+					String time = temp.substring(0, index);
+					System.out.println(time);
+					index = temp.indexOf("T");
+					String first = time.substring(index, index + "17:49:05".length());
+					String second = "</br>";
+					String third = time.substring(0, "2021-04-18".length());
+					result = first + second + third;
+				}
+			}
+			// status
+//    		temp = temp.substring(index + 1);
+//    		index = temp.indexOf(" ");
+//    		String status = temp.substring(0, index);
+
+//    		//image name
+//    		String imageName = null;
+//    		if (status.equals("on")) {
+//	    		temp = temp.substring(index + 1);
+//	    		imageName = temp;
+//    		}
+		}
+		return result;
 	}
 
 	private void forward(String PAGE, HttpServletRequest request, HttpServletResponse response)
@@ -86,6 +146,10 @@ public class ClientServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	public static void main(String[] args) {
+		//System.out.println(getLastLoggedOn("00:0c:29:a3:fc:19"));
 	}
 
 }
